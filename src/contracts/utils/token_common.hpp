0fcf69fbd1a005024a4c1ddd60a9bc03337c7a13
Sergey Ilin <ilinserg@gmail.com>
2020-03-09 22:28:02 -0500

    Resetting test envionrment

--- src/contracts/utils/token_common.hpp
+++ src/contracts/utils/token_common.hpp
@@ -4,7 +4,7 @@
 
 #if STAGE == 1
 int PERIOD_LENGTH = 7200;               // 2 hours
-time START_PERIOD_TIME = 1579046400UL;  // Wed, 15 Jan 2020 00:00:00
+time START_PERIOD_TIME = 1583020800UL;  // Wed, 15 Jan 2020 00:00:00
 #elif STAGE == 2
 int PERIOD_LENGTH = 3;          // 3 sec
 

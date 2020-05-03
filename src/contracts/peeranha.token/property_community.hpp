f14c9c08a6f88725e9e9b91a0fd121c0b8eb5993
freitag-fri <sanya007ua@gmail.com>
2020-05-03 21:45:59 +0300

    create class property community

---
+++ src/contracts/peeranha.token/property_community.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <eosio/eosio.hpp>
+#include <vector>
+
+struct [[eosio::table("propertycomm"), eosio::contract("peeranha.main")]] property_community {
+  eosio::name user;
+  std::vector<key_communit> properties;
+  uint64_t primary_key() const { return user.value; }
+};
+typedef eosio::multi_index<"propertycomm"_n, property_community> property_community_index;
+const uint64_t scope_all_property_community = eosio::name("allpropertycomm").value;
+
+struct key_community{
+  int community;
+  int value;
+  bool operator == (const int& other){
+    return community == other;
+  }
+}

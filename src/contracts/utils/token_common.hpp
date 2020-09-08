1adbb4fecd4152fbee370414fcf246af2858e9bf
freitag-fri <sanya007ua@gmail.com>
2020-09-08 17:38:27 +0300

    update START_PERIOD_TIME

--- src/contracts/utils/token_common.hpp
+++ src/contracts/utils/token_common.hpp
@@ -4,7 +4,7 @@
 
 #if STAGE == 1
 int PERIOD_LENGTH = 7200;               // 2 hours
-time START_PERIOD_TIME = 1598953801UL;  // Wed, September 1, 2020 12:50:01 PM GMT+03:00 DST
+time START_PERIOD_TIME = 1599571209UL;  // Wed, September 8, 2020 4:20:09 PM GMT+03:00 DST                                                
 #elif STAGE == 2
 int PERIOD_LENGTH = 3;          // 3 sec
 

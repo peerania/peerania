c6214632b0b43a5c9f5ab23286b0d4c6581b7045
freitag-fri <sanya007ua@gmail.com>
2020-11-24 19:23:07 +0200

    corrected2

--- src/contracts/peeranha.main/peeranha_vote.cpp
+++ src/contracts/peeranha.main/peeranha_vote.cpp
@@ -72,18 +72,19 @@ void peeranha::vote_forum_item(eosio::name user, uint64_t question_id,
         }
       });
   
-  update_rating(iter_account, caller_rating_change,
-                [energy](auto &account) { 
-                  account.reduce_energy(energy);
-                });
-
-  auto iter_answer_account = find_account(target_user);
+    auto iter_answer_account = find_account(target_user);
   int32_t sum_answer_15_minutes = get_property_d(iter_answer_account->integer_properties, PROPERTY_ANSWER_15_MINUTES, 0) + within_15_minutes;
   if (within_15_minutes)
     update_achievement(iter_answer_account->user, ANSWER_15_MINUTES, sum_answer_15_minutes);
   int32_t sum_first_answer = get_property_d(iter_answer_account->integer_properties, PROPERTY_FIRST_ANSWER, 0) + first_answer;
   if (first_answer)
     update_achievement(iter_answer_account->user, FIRST_ANSWER, sum_first_answer);
+  
+  update_rating(iter_account, caller_rating_change,
+                [energy](auto &account) { 
+                  account.reduce_energy(energy);
+                });
+
   update_rating(iter_answer_account, target_user_rating_change,
                 [sum_answer_15_minutes, within_15_minutes, sum_first_answer, first_answer](auto &account) {
                   if (within_15_minutes)

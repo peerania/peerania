e5bb25b5cb19c3ab179ccd2df820a5a78b443a20
freitag-fri <sanya007ua@gmail.com>
2020-12-03 15:33:35 +0200

    update unitest

--- src/unittests/contracts/test_account_moderator_flag.py
+++ src/unittests/contracts/test_account_moderator_flag.py
@@ -90,13 +90,10 @@ class AccountModerationTests(peeranhatest.peeranhaTest):
         self.action('reportforum', {'user': 'ted', 'question_id': AQ_id, 'answer_id': 0, 'comment_id': 0},
                     ted, 'Ted delete Alice question->Bob answer->Carol comment')
         new_rating = base_rating
-        new_rating['alice_rating'] += economy['QUESTION_DELETED_REWARD']
-        new_rating['bob_rating'] += economy['ANSWER_DELETED_REWARD'] - economy['ANSWER_UPVOTED_REWARD'] - economy['ANSWER_UPVOTED_REWARD'] #firt answer - answer withing 15 minutes
+        new_rating['alice_rating'] += economy['QUESTION_DELETED_REWARD'] 
+        new_rating['bob_rating'] += economy['ANSWER_DELETED_REWARD'] - economy['ANSWER_UPVOTED_REWARD'] * 0.5 - economy['ANSWER_UPVOTED_REWARD'] * 0.5 #firt answer - answer withing 15 minutes
         new_rating['carol_rating'] += economy['COMMENT_DELETED_REWARD']
         setvar(account_e, new_rating)
-        print(account_e)
-        print("===============")
-        print(self.table('account', 'allaccounts'))
         self.assertTrue(compare(account_e, self.table(
             'account', 'allaccounts'), ignore_excess=True))
         self.assertTrue(len(self.table('question', 'allquestions')) == 0)

d81c528888d01e19fad78a0e1bc91b2ae5d99e33
YuriyLu <yura2000uraura@gmail.com>
2021-03-09 13:58:12 +0300

    Delete question type changing in modquestion action

--- src/contracts/peeranha.main/peeranha_forum.cpp
+++ src/contracts/peeranha.main/peeranha_forum.cpp
@@ -306,13 +306,10 @@ void peeranha::modify_question(eosio::name user, uint64_t question_id,
                                uint16_t community_id,
                                const std::vector<uint32_t> &tags,
                                const std::string &title,
-                               const IpfsHash &ipfs_link,
-                               const uint8_t type) {
+                               const IpfsHash &ipfs_link) {
   assert_ipfs(ipfs_link);
   assert_title(title);
-  assert_question_type(type);
   assert_community_exist(community_id);
-  assert_community_questions_type(community_id, type);
   auto iter_account = find_account(user);
   auto iter_question = find_question(question_id);
   assert_allowed(*iter_account, iter_question->user, Action::MODIFY_QUESTION, community_id);
@@ -326,14 +323,12 @@ void peeranha::modify_question(eosio::name user, uint64_t question_id,
   update_tags_statistics(community_id, tags, 1);
   question_table.modify(
       iter_question, _self,
-      [&ipfs_link, &title, community_id, &tags, type](auto &question) {
+      [&ipfs_link, &title, community_id, &tags](auto &question) {
         question.ipfs_link = ipfs_link;
         question.title = title;
         question.community_id = community_id;
         question.tags = tags;
         set_property(question.properties, PROPERTY_LAST_MODIFIED, now());
-        set_property_d(question.properties, PROPERTY_QUESTION_TYPE, (int)type,
-                   QUESTION_TYPE_EXPERT);
       });
   update_rating(iter_account, 0, [](auto &account) {
     account.reduce_energy(ENERGY_MODIFY_QUESTION);
